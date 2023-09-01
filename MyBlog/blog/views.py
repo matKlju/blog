1312a6d245f7b66397eebc6ce8769d080141f03c
matKlju <108662553+matKlju@users.noreply.github.com>
2023-09-01 11:36:10 +0300

    Edited base.html to show my name Added pagination functionality limit set to 8 - edited pagination functionality to all_posts.html file

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -7,6 +7,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 8
 
 
 class PostDetailView(DetailView):
